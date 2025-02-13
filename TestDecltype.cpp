d0e8ccb0708169d697b72df08449963f6079049d
Mario Foos <mariofoos@gmail.com>
2025-02-13 08:11:33 -0300

    - Primer commit

---
+++ TestDecltype.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+// Name        : TestLambda.cpp
+// Author      : Mario Foos
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T, typename U> auto sumar(T a, U b) -> decltype(a + b);
+
+int main()
+{
+	int num1 = 7;
+	decltype(num1) num2 = 13;  // num2 es de tipo int, igual que num1.
+
+	cout << "num2: " << num2 << endl;
+
+
+	double x = 3.14;
+	int y = 2;
+	// Se infiere el tipo de (x + y), que es double, para usarlo como tipo para z
+	decltype(x + y) z = x + y;
+
+	cout << "z: " << z << endl;
+
+    std::vector<int> numeros = {1, 2, 3, 4, 5};
+    // Obtenemos el tipo del iterador del vector
+    decltype(numeros.begin()) it = numeros.begin();
+
+    cout << "Primer elemento: " << *it << endl;
+
+	return 0;
+}
+
+template <typename T, typename U> auto sumar(T a, U b) -> decltype(a + b)
+{
+    return a + b;
+}
+
+
